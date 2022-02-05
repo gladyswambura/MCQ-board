5e30d125da201248e634ce27f1d86f1f3c6aac26
gladyswambura <gladyswahito7@gmail.com>
2022-02-06 02:23:52 +0300

    THis commit adds a submit button at the end of our quizzes on the webpage

---
+++ Js/sandbox.js
@@ -0,0 +1,10 @@
+let userForm=document.getElementById("userForm");
+userForm.addEventListener("submit", function(event){
+    alert("Form Submitted")
+event.preventDefault()
+let userData=newFormData(userForm))
+}
+let male=userData.get(male)
+alert(male)
+let male=userData.get(male)
+alert(male)
