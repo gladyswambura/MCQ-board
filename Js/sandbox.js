7ecdc01423e470c2932c9cf19aebee8fa338e621
gladyswambura <gladyswahito7@gmail.com>
2022-02-06 11:21:29 +0300

    This commit completes styling page one of my website

--- Js/sandbox.js
+++ Js/sandbox.js
@@ -1,26 +1,42 @@
-let userForm=document.getElementById("userForm");
-userForm.addEventListener("submit", function(event){
-    alert("Form Submitted")
-event.preventDefault()
-let userData=newFormData(userForm))
-}
-let male=userData.get(male)
-if(male ===correctAnswer("Tim Berners-Lee"){
-alert(correct!)}
-else{
-    alert(wrong! Try again!)
+function submit(){
+    alert("Form submitted")
 }
 
-let language=userData.get(language)
 
-alert(language)
 
-let what=userData.get(what)
-alert(what)
 
-let father=userData.get(father)
-alert(lfather)
+// let userForm=document.getElementById("userForm");
+// userForm.addEventListener("submit", function(event){
+//     alert("Form Submitted")
+// event.preventDefault()
+// let userData=newFormData(userForm))
+// }
+// let male=userData.get(male)
+// if(male ===correctAnswer("Tim Berners-Lee"){
+// alert(correct!)}
+// else{
+//     alert(wrong! Try again!)
+// }
+// let language=userData.get(language)
+// if(language ===correctAnswer("Tim Berners-Lee"){
+//     alert(correct!)}
+//     else{
+//         alert(wrong! Try again!)
+// let what=userData.get(what)
+// if(what ===correctAnswer("Tim Berners-Lee"){
+//     alert(correct!)}
+//     else{
+//         alert(wrong! Try again!)
 
-let model=userData.get(model)
-alert(model)
- 
+// let father=userData.get(father)
+// if(father ===correctAnswer("Tim Berners-Lee"){
+//     alert(correct!)}
+//     else{
+//         alert(wrong! Try again!)
+
+// let model=userData.get(model)
+// if(model ===correctAnswer("Tim Berners-Lee"){
+//     alert(correct!)}
+//     else{
+//         alert(wrong! Try again!)
+//     }
