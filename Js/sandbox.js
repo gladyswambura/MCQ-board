46cc45de951f6eb22a857735926f506b85353f4f
gladyswambura <gladyswahito7@gmail.com>
2022-02-06 16:05:26 +0300

    This commit activates the submit button

--- Js/sandbox.js
+++ Js/sandbox.js
@@ -1,42 +1,51 @@
-function submit(){
-    alert("Form submitted")
+let userForm = document.getElementById("userForm");
+let score = 0;
+userForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let userData = new FormData(userForm);
+    questions(userData);
+})
+
+function questions(userData) {
+    let male = userData.get("male");
+    if (male == "Tim Berners-Lee") {
+        score++;
+        // alert(score)
+    } else {
+        alert("wrong! Try again!");
+    }
+    let language = userData.get("language");
+    if (language == "Markup Language") {
+        score++;
+        // alert(score)
+    } else {
+        alert("wrong! Try again!");
+    }
+    let what = userData.get("what");
+    if (what == "css") {
+        score++;
+        // alert("correct!")
+    } else {
+        alert("wrong! Try again!");
+    }
+
+    let father = userData.get("father");
+    if (father == "Watts S. Humphre") {
+        score++;
+        // alert("correct!")
+    } else {
+        alert("wrong! Try again!");
+    }
+    let model = userData.get("model");
+    if (model == "4GT Model") {
+        score++;
+        // alert("correct!")
+    } else {
+        alert("wrong! Try again!");
+    }
+    alert(score);
+    let userScore = document.querySelector(".score");
+    userScore.textContent = score;
+    // userScore.getElementsByClassName.color="red";
+    // userForm.style.display="none";
 }
-
-
-
-
-// let userForm=document.getElementById("userForm");
-// userForm.addEventListener("submit", function(event){
-//     alert("Form Submitted")
-// event.preventDefault()
-// let userData=newFormData(userForm))
-// }
-// let male=userData.get(male)
-// if(male ===correctAnswer("Tim Berners-Lee"){
-// alert(correct!)}
-// else{
-//     alert(wrong! Try again!)
-// }
-// let language=userData.get(language)
-// if(language ===correctAnswer("Tim Berners-Lee"){
-//     alert(correct!)}
-//     else{
-//         alert(wrong! Try again!)
-// let what=userData.get(what)
-// if(what ===correctAnswer("Tim Berners-Lee"){
-//     alert(correct!)}
-//     else{
-//         alert(wrong! Try again!)
-
-// let father=userData.get(father)
-// if(father ===correctAnswer("Tim Berners-Lee"){
-//     alert(correct!)}
-//     else{
-//         alert(wrong! Try again!)
-
-// let model=userData.get(model)
-// if(model ===correctAnswer("Tim Berners-Lee"){
-//     alert(correct!)}
-//     else{
-//         alert(wrong! Try again!)
-//     }
